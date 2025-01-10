61c3bc4b00f40476ff5832759bc8fa1da9f630bf
nikulka <nika98love@mail.ru>
2025-01-10 23:11:23 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Petr Arsentev");
+        System.out.println("01.12.2012");
+    }
+}
